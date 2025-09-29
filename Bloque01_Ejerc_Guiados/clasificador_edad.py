cbf2a0869fce3e4f56e8c82d2e227043911e194b
Alvaro <alvarosanchez1708@gmail.com>
2025-09-29 13:21:09 +0200

    2

---
+++ Bloque01_Ejerc_Guiados/clasificador_edad.py
@@ -0,0 +1,8 @@
+edad = input("Introduce tu edad:")
+edad = int(edad)
+if edad < 18:
+    print ("Eres menor de edad")
+elif 18 <= edad <= 65:
+    print("Eres adulto")
+else:
+    print("Eres adulto mayor")
