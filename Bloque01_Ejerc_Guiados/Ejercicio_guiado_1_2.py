a43d2f82a3c44ce2a136c5ec3859c9aa63ed3b1c
Alvaro <alvarosanchez1708@gmail.com>
2025-09-29 12:37:21 +0200

    2

--- Bloque01_Ejerc_Guiados/Ejercicio_guiado_1_2.py
+++ Bloque01_Ejerc_Guiados/Ejercicio_guiado_1_2.py
@@ -5,4 +5,4 @@ estudios = input("¿Estudias? (True/False) ")
 
 
 
-print(f"Me llamo, {nombre}, tengo {edad} años, mido{altura} metros y actualmente estudio:")
+print(f"Me llamo, {nombre}, tengo {edad} años, mido{altura} metros y actualmente estudio: {estudios}")
