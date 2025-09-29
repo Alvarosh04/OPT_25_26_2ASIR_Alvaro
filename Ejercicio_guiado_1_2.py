49a6f60fb756682efe86051f1e5d45a4a2077068
Alvaro <alvarosanchez1708@gmail.com>
2025-09-29 11:43:22 +0200

    2

---
+++ Ejercicio_guiado_1_2.py
@@ -0,0 +1,8 @@
+nombre = input("¿Como te llamas? ")
+edad = input("¿Que edad tienes? ")
+altura = input("¿Cuanto mides?")
+estudios = input("¿Estudias? (True/False) ")
+
+
+
+print(f"Me llamo, {nombre}, tengo {edad} años, mido{altura} metros y actualmente estudio:")
