5d9cec0f48c716a1a168ce3dc8ccdaca5f937e56
Alvaro <alvarosanchez1708@gmail.com>
2025-09-29 12:09:38 +0200

    2

---
+++ Bloque_01_Ejercicios_Ev/b1_2_calculadora_simple.py
@@ -0,0 +1,9 @@
+num1 = 18
+num2 = 4
+
+print(num1 + num2 ) #suma
+print(num1 - num2)   #Resta
+print(num1 * num2)  #Multiplicación
+print(num1 / num2 ) #Division
+print(num1 % num2) #Modulo
+print(num1 ** num2) #Potencia
---
+++ Bloque_01_Ejercicios_Ev/b1_3_conversor_datos.py
@@ -0,0 +1,4 @@
+nombre = input("¿Como te llamas?")
+edad = input("¿Que edad tienes?")
+altura = input("¿Cuanto mides?")
+print(f"Hola, me llamo {nombre}, tengo {edad} años y mido {altura} metros.")
